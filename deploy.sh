504f3516833139e51b86a77e4993b7a7eb645d7b
折影轻梦 <776194970@qq.com>
2019-03-10 15:17:40 +0800

    ee

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,8 @@ npm run docs:build
  
 # 进入生成的文件夹
 cd docs/.vuepress/dist
-# git init 官网是有这行的，但我的项目里给注释掉了，因为我是现有项目，不想每次都初始化
+
+git init
  
 # 下面这行是官网没有的，如果是多人开发项目，务必执行此命令
 git pull https://github.com/tncee/chinese-recite.git master 
