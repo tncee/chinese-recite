76ee0035fe2255911eba92fbd1f6cb16b10dab1c
折影轻梦 <776194970@qq.com>
2019-03-14 23:47:07 +0800

    描述优化

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,13 +3,13 @@ module.exports = {
   description: '义务教育中必须背诵的古诗词',
   base: '/chinese-recite/',
   themeConfig: {
-    lastUpdated: '最后编辑：',
+    lastUpdated: '最后编辑',
     
     // 假定是 GitHub. 同时也可以是一个完整的 GitLab URL
     repo: 'tncee/chinese-recite',
     // 自定义仓库链接文字。默认从 `themeConfig.repo` 中自动推断为
     // "GitHub"/"GitLab"/"Bitbucket" 其中之一，或是 "Source"。
-    repoLabel: '查看源码',
+    repoLabel: 'GitHub',
 
     // 以下为可选的编辑链接选项
 
